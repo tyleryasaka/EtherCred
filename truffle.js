71af1660e34716c0104df078d42d594504e7b4a2
Tyler Yasaka <tyler@partcycle.com>
2016-12-30 01:18:55 -0500

    Remove build property from truffle config, because we're using webpack now

--- truffle.js
+++ truffle.js
@@ -1,13 +1,4 @@
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ]
-  },
   rpc: {
     host: "localhost",
     port: 8545
