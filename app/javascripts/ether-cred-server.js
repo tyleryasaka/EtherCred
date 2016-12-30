b04362435e81aede8aa88c72d56f34ac79a6ea22
Tyler Yasaka <tyler@partcycle.com>
2016-12-29 20:47:05 -0500

    Creates an adapter for the solidity contract returned by truffle

---
+++ app/javascripts/ether-cred-server.js
@@ -0,0 +1,44 @@
+const blankAddress = '0x0000000000000000000000000000000000000000';
+
+// TODO: test this
+function EtherCredServer(deployedContract) {
+    this.getApprovalsFor = function(requesterAddress) {
+        return deployedContract.getApprovalsFor(requesterAddress).then(function(approvals) {
+            var filteredApprovals = [];
+            for(var a = 0; a < approvals.length; a++) {
+                if(approvals[a] !== blankAddress) {
+                    filteredApprovals.push(approvals[a]);
+                }
+            }
+            return filteredApprovals;
+        });
+    };
+
+    this.approve = function(requesterAddress, targetAddress) {
+        return deployedContract.approve(targetAddress, {from: requesterAddress});
+    };
+
+    this.unapprove = function(requesterAddress, targetAddress) {
+        return deployedContract.unapprove(targetAddress, {from: requesterAddress});
+    };
+
+    this.getDisapprovalsFor = function(requesterAddress) {
+        return deployedContract.getDisapprovalsFor(requesterAddress).then(function(disapprovals) {
+            var filteredDisapprovals = [];
+            for(var a = 0; a < disapprovals.length; a++) {
+                if(disapprovals[a] !== blankAddress) {
+                    filteredDisapprovals.push(disapprovals[a]);
+                }
+            }
+            return filteredDisapprovals;
+        });
+    };
+
+    this.disapprove = function(requesterAddress, targetAddress) {
+        return deployedContract.disapprove(targetAddress, {from: requesterAddress});
+    };
+
+    this.undisapprove = function(requesterAddress, targetAddress) {
+        return deployedContract.undisapprove(targetAddress, {from: requesterAddress});
+    };
+}
