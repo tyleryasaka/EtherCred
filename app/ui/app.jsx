afc1ca47b4354033b29fc1f179cb13280296ff6e
Tyler Yasaka <tyler@partcycle.com>
2017-01-01 03:22:06 -0500

    Implements interfave to create approval

--- app/ui/app.jsx
+++ app/ui/app.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 
+class NewApproval extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {address: ''};
+
+        this.approve = this.approve.bind(this)
+        this.onChangeAddress = this.onChangeAddress.bind(this)
+    }
+
+    onChangeAddress(event) {
+        this.setState({address: event.target.value});
+    }
+
+    approve() {
+        this.props.onApprove(this.state.address);
+        this.setState({address: ''});
+    }
+
+    render(){
+        return (
+            <div>
+                <input type="text" placeholder="New approval..." value={this.state.address}
+                    onChange={this.onChangeAddress}/>
+                <button onClick={this.approve}>Add</button>
+            </div>
+        )
+    }
+}
+
 class Approval extends React.Component {
+    constructor(props) {
+        super(props);
+        this.unapprove = this.unapprove.bind(this)
+    }
+
     unapprove() {
         this.props.onUnapprove(this.props.approval);
     }
 
     render(){
-        return <li>{this.props.approval} <button onClick={this.unapprove.bind(this)}>delete</button></li>
+        return <li>{this.props.approval} <button onClick={this.unapprove}>delete</button></li>
     }
 }
 
@@ -28,9 +62,17 @@ class Approvals extends React.Component {
 
 class App extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {approvals: props.user.approvals};
-      this.unapprove = this.unapprove.bind(this); // this line needed?
+        super(props);
+        this.state = {approvals: this.props.user.approvals};
+
+        this.unapprove = this.unapprove.bind(this);
+        this.approve = this.approve.bind(this);
+    }
+
+    approve(target) {
+        this.props.user.approve(target).then(() => {
+            this.setState({approvals: this.props.user.approvals});
+        });
     }
 
     unapprove(target) {
@@ -44,6 +86,7 @@ class App extends React.Component {
             <div>
                 <p>Your ethereum address is: {this.props.user.address}</p>
                 <h3>Your Approvals:</h3>
+                <NewApproval onApprove={this.approve}/>
                 <Approvals approvals={this.state.approvals}
                     onUnapprove={this.unapprove}/>
             </div>
