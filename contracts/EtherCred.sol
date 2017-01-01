18e5b12e40896ef0f3c12d560e629d70e2655adf
Tyler Yasaka <tyler@partcycle.com>
2016-12-31 19:56:39 -0500

    Refactors the contract to not run a `delete` on an address in storage. This throws an unknown error (actually just responds "out of gas"). Instead of deleting the address in storage, it just marks it inactive, and then returns empty slots instead of inactive addresses when fetching them. The API is identical, but the implentation is changed.
    I suspect the error is just with testrpc, so this may not have been necessary for a live deployment. That said I'm not sure and for now this is the simplest reliable solution.

--- contracts/EtherCred.sol
+++ contracts/EtherCred.sol
@@ -1,38 +1,56 @@
 pragma solidity ^0.4.2;
 
 contract EtherCred {
+
+    struct CitizenReference {
+        address id;
+        bool isActive;
+    }
+
     struct Citizen {
-        address[] approvals;
-        address[] disapprovals;
+        CitizenReference[] approvals;
+        CitizenReference[] disapprovals;
     }
 
     mapping(address => Citizen) citizens;
 
     function approve(address _target) {
-        citizens[msg.sender].approvals.push(_target);
+        citizens[msg.sender].approvals.push(CitizenReference(_target, true));
     }
 
     function unapprove(uint _index) {
         if(_index < citizens[msg.sender].approvals.length) {
-            delete citizens[msg.sender].approvals[_index];
+            citizens[msg.sender].approvals[_index].isActive = false;
         }
     }
 
     function getApprovalsFor(address _target) constant returns (address[]) {
-        return citizens[_target].approvals;
+        address[] memory activeAddresses = new address[](citizens[_target].approvals.length);
+        for (uint i = 0; i < citizens[_target].approvals.length; i++) {
+            if(citizens[_target].approvals[i].isActive) {
+                activeAddresses[i] = citizens[_target].approvals[i].id;
+            }
+        }
+        return activeAddresses;
     }
 
     function disapprove(address _target) {
-        citizens[msg.sender].disapprovals.push(_target);
+        citizens[msg.sender].disapprovals.push(CitizenReference(_target, true));
     }
 
     function undisapprove(uint _index) {
         if(_index < citizens[msg.sender].disapprovals.length) {
-            delete citizens[msg.sender].disapprovals[_index];
+            citizens[msg.sender].disapprovals[_index].isActive = false;
         }
     }
 
     function getDisapprovalsFor(address _target) constant returns (address[]) {
-        return citizens[_target].disapprovals;
+        address[] memory activeAddresses = new address[](citizens[_target].disapprovals.length);
+        for (uint i = 0; i < citizens[_target].disapprovals.length; i++) {
+            if(citizens[_target].disapprovals[i].isActive) {
+                activeAddresses[i] = citizens[_target].disapprovals[i].id;
+            }
+        }
+        return activeAddresses;
     }
 }
